07fa05d397ad8484063f2eebaacd96502b295536
ixaxaar <root@ixaxaar.in>
2024-05-14 23:21:59 +0530

    new module structure for i2c

--- src/I2Cctl.cpp
+++ src/I2Cctl.cpp
@@ -1,15 +1,148 @@
 #include "I2Cctl.h"
 
-I2CCtl::I2CCtl(int sdaPin, int sclPin, uint32_t frequency)
+I2CCtl::I2CCtl() : _sdaPin(0), _sclPin(0), _frequency(100000) {}
+
+void I2CCtl::init(const std::vector<std::pair<std::string, std::string>> &params)
 {
-    _sdaPin = sdaPin;
-    _sclPin = sclPin;
-    _frequency = frequency;
+    for (const auto &param : params)
+    {
+        if (param.first == "sdaPin")
+        {
+            _sdaPin = std::stoi(param.second);
+        }
+        else if (param.first == "sclPin")
+        {
+            _sclPin = std::stoi(param.second);
+        }
+        else if (param.first == "frequency")
+        {
+            _frequency = std::stoul(param.second);
+        }
+    }
+
+    Wire.begin(_sdaPin, _sclPin);
+    setClock(_frequency);
 }
 
-void I2CCtl::begin()
+void I2CCtl::deinit()
 {
-    Wire.begin(_sdaPin, _sclPin, _frequency);
+    Wire.end();
+}
+
+std::pair<std::string, void *> I2CCtl::execute(const std::string &command, const std::vector<std::pair<std::string, std::string>> &params)
+{
+    if (command == "setClock")
+    {
+        uint32_t frequency = 0;
+        for (const auto &param : params)
+        {
+            if (param.first == "frequency")
+            {
+                frequency = std::stoul(param.second);
+                break;
+            }
+        }
+        setClock(frequency);
+        return {"", nullptr};
+    }
+    else if (command == "beginTransmission")
+    {
+        uint8_t address = 0;
+        for (const auto &param : params)
+        {
+            if (param.first == "address")
+            {
+                address = std::stoul(param.second);
+                break;
+            }
+        }
+        beginTransmission(address);
+        return {"", nullptr};
+    }
+    else if (command == "endTransmission")
+    {
+        bool stopBit = true;
+        for (const auto &param : params)
+        {
+            if (param.first == "stopBit")
+            {
+                stopBit = (param.second == "true");
+                break;
+            }
+        }
+        uint8_t *result = endTransmission(stopBit);
+        return {"uint8_t", static_cast<void *>(result)};
+    }
+    else if (command == "requestFrom")
+    {
+        uint8_t address = 0;
+        uint8_t quantity = 0;
+        bool stopBit = true;
+        for (const auto &param : params)
+        {
+            if (param.first == "address")
+            {
+                address = std::stoul(param.second);
+            }
+            else if (param.first == "quantity")
+            {
+                quantity = std::stoul(param.second);
+            }
+            else if (param.first == "stopBit")
+            {
+                stopBit = (param.second == "true");
+            }
+        }
+        uint8_t *result = requestFrom(address, quantity, stopBit);
+        return {"uint8_t", static_cast<void *>(result)};
+    }
+    else if (command == "write")
+    {
+        if (params.size() == 1 && params[0].first == "data")
+        {
+            uint8_t data = std::stoul(params[0].second);
+            write(data);
+        }
+        else if (params.size() == 2 && params[0].first == "data" && params[1].first == "quantity")
+        {
+            size_t quantity = std::stoul(params[1].second);
+            uint8_t *data = new uint8_t[quantity];
+            for (size_t i = 0; i < quantity; ++i)
+            {
+                data[i] = std::stoul(params[0].second);
+            }
+            write(data, quantity);
+            delete[] data;
+        }
+        return {"", nullptr};
+    }
+    else if (command == "read")
+    {
+        size_t quantity = 0;
+        for (const auto &param : params)
+        {
+            if (param.first == "quantity")
+            {
+                quantity = std::stoul(param.second);
+                break;
+            }
+        }
+        uint8_t *result = read(quantity);
+        return {"uint8_t[]", static_cast<void *>(result)};
+    }
+    return {"", nullptr};
+}
+
+std::vector<FunctionInfo> I2CCtl::getSupportedFunctions()
+{
+    return {
+        {"setClock", {{"frequency", "uint32_t"}}},
+        {"beginTransmission", {{"address", "uint8_t"}}},
+        {"endTransmission", {{"stopBit", "bool"}}},
+        {"requestFrom", {{"address", "uint8_t"}, {"quantity", "uint8_t"}, {"stopBit", "bool"}}},
+        {"write", {{"data", "uint8_t"}}},
+        {"write", {{"data", "uint8_t"}, {"quantity", "size_t"}}},
+        {"read", {{"quantity", "size_t"}}}};
 }
 
 void I2CCtl::setClock(uint32_t frequency)
@@ -23,14 +156,16 @@ void I2CCtl::beginTransmission(uint8_t address)
     Wire.beginTransmission(address);
 }
 
-uint8_t I2CCtl::endTransmission(bool stopBit)
+uint8_t *I2CCtl::endTransmission(bool stopBit)
 {
-    return Wire.endTransmission(stopBit);
+    uint8_t *result = new uint8_t(Wire.endTransmission(stopBit));
+    return result;
 }
 
-uint8_t I2CCtl::requestFrom(uint8_t address, uint8_t quantity, bool stopBit)
+uint8_t *I2CCtl::requestFrom(uint8_t address, uint8_t quantity, bool stopBit)
 {
-    return Wire.requestFrom(address, quantity, stopBit);
+    uint8_t *result = new uint8_t(Wire.requestFrom(address, quantity, stopBit));
+    return result;
 }
 
 void I2CCtl::write(uint8_t data)
@@ -43,12 +178,12 @@ void I2CCtl::write(const uint8_t *data, size_t quantity)
     Wire.write(data, quantity);
 }
 
-uint8_t I2CCtl::available()
+uint8_t *I2CCtl::read(size_t quantity)
 {
-    return Wire.available();
-}
-
-uint8_t I2CCtl::read()
-{
-    return Wire.read();
+    uint8_t *data = new uint8_t[quantity];
+    for (size_t i = 0; i < quantity; ++i)
+    {
+        data[i] = Wire.read();
+    }
+    return data;
 }
--- src/I2Cctl.h
+++ src/I2Cctl.h
@@ -1,27 +1,34 @@
+
 #ifndef I2C_H
 #define I2C_H
 
 #include <Arduino.h>
 #include <Wire.h>
+#include <vector>
+#include <string>
+#include "module.h"
 
-class I2CCtl
+class I2CCtl : public ModuleInterface
 {
 public:
-    I2CCtl(int sdaPin, int sclPin, uint32_t frequency = 100000);
-    void begin();
-    void setClock(uint32_t frequency);
-    void beginTransmission(uint8_t address);
-    uint8_t endTransmission(bool stopBit = true);
-    uint8_t requestFrom(uint8_t address, uint8_t quantity, bool stopBit = true);
-    void write(uint8_t data);
-    void write(const uint8_t *data, size_t quantity);
-    uint8_t available();
-    uint8_t read();
+    I2CCtl();
+    void init(const std::vector<std::pair<std::string, std::string>> &params) override;
+    void deinit() override;
+    std::pair<std::string, void *> execute(const std::string &command, const std::vector<std::pair<std::string, std::string>> &params) override;
+    std::vector<FunctionInfo> getSupportedFunctions() override;
 
 private:
     int _sdaPin;
     int _sclPin;
     uint32_t _frequency;
+
+    void setClock(uint32_t frequency);
+    void beginTransmission(uint8_t address);
+    uint8_t *endTransmission(bool stopBit);
+    uint8_t *requestFrom(uint8_t address, uint8_t quantity, bool stopBit);
+    void write(uint8_t data);
+    void write(const uint8_t *data, size_t quantity);
+    uint8_t *read(size_t quantity);
 };
 
 #endif // I2C_H
