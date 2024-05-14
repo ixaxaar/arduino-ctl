772042f39b7f9ceefad214c4a06d00dd757605dc
ixaxaar <root@ixaxaar.in>
2024-05-14 23:19:59 +0530

    new module structure for gpio

--- src/gpioctl.cpp
+++ src/gpioctl.cpp
@@ -1,14 +1,100 @@
+
 #include "gpioctl.h"
 
-GPIOCtl::GPIOCtl(int pin, int mode)
+GPIOCtl::GPIOCtl() : _pin(0), _mode(0) {}
+
+void GPIOCtl::init(const std::vector<std::pair<std::string, std::string>> &params)
 {
-    _pin = pin;
-    _mode = mode;
+    for (const auto &param : params)
+    {
+        if (param.first == "pin")
+        {
+            _pin = std::stoi(param.second);
+        }
+        else if (param.first == "mode")
+        {
+            _mode = std::stoi(param.second);
+        }
+    }
+
+    pinMode(_mode);
 }
 
-void GPIOCtl::begin()
+void GPIOCtl::deinit()
 {
-    pinMode(_mode);
+    // No specific deinitialization required for GPIO
+}
+
+std::pair<std::string, void *> GPIOCtl::execute(const std::string &command, const std::vector<std::pair<std::string, std::string>> &params)
+{
+    if (command == "pinMode")
+    {
+        int mode = 0;
+        for (const auto &param : params)
+        {
+            if (param.first == "mode")
+            {
+                mode = std::stoi(param.second);
+                break;
+            }
+        }
+        pinMode(mode);
+        return {"", nullptr};
+    }
+    else if (command == "digitalWrite")
+    {
+        int value = 0;
+        for (const auto &param : params)
+        {
+            if (param.first == "value")
+            {
+                value = std::stoi(param.second);
+                break;
+            }
+        }
+        digitalWrite(value);
+        return {"", nullptr};
+    }
+    else if (command == "digitalRead")
+    {
+        int *value = digitalRead();
+        return {"int", static_cast<void *>(value)};
+    }
+    else if (command == "attachInterrupt")
+    {
+        void (*callback)(void) = nullptr;
+        int mode = 0;
+        for (const auto &param : params)
+        {
+            if (param.first == "callback")
+            {
+                // Assuming the callback is passed as a function pointer
+                callback = reinterpret_cast<void (*)(void)>(std::stoul(param.second, nullptr, 16));
+            }
+            else if (param.first == "mode")
+            {
+                mode = std::stoi(param.second);
+            }
+        }
+        attachInterrupt(callback, mode);
+        return {"", nullptr};
+    }
+    else if (command == "detachInterrupt")
+    {
+        detachInterrupt();
+        return {"", nullptr};
+    }
+    return {"", nullptr};
+}
+
+std::vector<FunctionInfo> GPIOCtl::getSupportedFunctions()
+{
+    return {
+        {"pinMode", {{"mode", "int"}}},
+        {"digitalWrite", {{"value", "int"}}},
+        {"digitalRead", {}},
+        {"attachInterrupt", {{"callback", "void (*)(void)"}, {"mode", "int"}}},
+        {"detachInterrupt", {}}};
 }
 
 void GPIOCtl::pinMode(int mode)
@@ -22,9 +108,10 @@ void GPIOCtl::digitalWrite(int value)
     ::digitalWrite(_pin, value);
 }
 
-int GPIOCtl::digitalRead()
+int *GPIOCtl::digitalRead()
 {
-    return ::digitalRead(_pin);
+    int *value = new int(::digitalRead(_pin));
+    return value;
 }
 
 void GPIOCtl::attachInterrupt(void (*callback)(void), int mode)
--- src/gpioctl.h
+++ src/gpioctl.h
@@ -1,22 +1,30 @@
+
 #ifndef GPIO_H
 #define GPIO_H
 
 #include <Arduino.h>
+#include <vector>
+#include <string>
+#include "module.h"
 
-class GPIOCtl
+class GPIOCtl : public ModuleInterface
 {
 public:
-    GPIOCtl(int pin, int mode);
-    void begin();
-    void pinMode(int mode);
-    void digitalWrite(int value);
-    int digitalRead();
-    void attachInterrupt(void (*callback)(void), int mode);
-    void detachInterrupt();
+    GPIOCtl();
+    void init(const std::vector<std::pair<std::string, std::string>> &params) override;
+    void deinit() override;
+    std::pair<std::string, void *> execute(const std::string &command, const std::vector<std::pair<std::string, std::string>> &params) override;
+    std::vector<FunctionInfo> getSupportedFunctions() override;
 
 private:
     int _pin;
     int _mode;
+
+    void pinMode(int mode);
+    void digitalWrite(int value);
+    int *digitalRead();
+    void attachInterrupt(void (*callback)(void), int mode);
+    void detachInterrupt();
 };
 
 #endif // GPIO_H
